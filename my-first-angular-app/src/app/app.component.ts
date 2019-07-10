d6c89d43edae1c3b1e88800b2fbbab16f09d145c
ashygoyal <goyal.ashish3@gmail.com>
2019-07-11 03:05:16 +0530

    Till FilterTextbox_AngularComponents&Modules

--- my-first-angular-app/src/app/app.component.ts
+++ my-first-angular-app/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'my-first-angular-app';
+  constructor(){}
+
+  title: string;
+  
+  ngOnInit(){
+    this.title = 'Hello Peeps, this is my-first-angular-app';
+  }
+
 }
