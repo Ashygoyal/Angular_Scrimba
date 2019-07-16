6ea35373f56b4028e7167360b969280ab104577e
ashygoyal <goyal.ashish3@gmail.com>
2019-07-16 23:38:42 +0530

    Till Adding Filtering

--- my-first-angular-app/src/app/customers/customers-list/customers-list.component.ts
+++ my-first-angular-app/src/app/customers/customers-list/customers-list.component.ts
@@ -42,11 +42,27 @@ export class CustomersListComponent implements OnInit {
       this.customersOrderTotal = 0;
       this.filteredCustomers.forEach((cust: ICustomer)=> {
           this.customersOrderTotal += cust.orderTotal;
-      });
+                                                                                                                                
+    });
   }
 
   sort(prop: string){
 
+  }
+
+  filter(data:string){
+    if(data){
+      this.filteredCustomers = this.customers.filter ((cust: ICustomer)=> {
+        return cust.name.toLowerCase().indexOf(data.toLowerCase())> -1 ||
+        cust.city.toLowerCase().indexOf(data.toLowerCase()) > -1 ||
+        cust.orderTotal.toString().indexOf(data) > -1;
+      })
+
+    }
+    else {
+      this.filteredCustomers = this.customers;
+    }
+    this.calculateOrders();
   }
 
 }
--- my-first-angular-app/src/app/customers/customers.module.ts
+++ my-first-angular-app/src/app/customers/customers.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { CustomersComponent } from './customers.component';
 import { CustomersListComponent } from './customers-list/customers-list.component';
@@ -11,7 +12,7 @@ import { SharedModule } from './../shared/shared.module';
     CustomersComponent, CustomersListComponent, FilterTextboxComponent
   ],
   imports: [
-    CommonModule, SharedModule
+    CommonModule, SharedModule, FormsModule
   ],
   exports: [
       CustomersComponent
